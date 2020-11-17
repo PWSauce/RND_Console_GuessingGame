f6fb83e6c5d90f61b61951598b457cf218cd15fe
PWSauce <dj@netatonce.net>
2020-11-17 17:40:24 +0100

    Initial commit

---
+++ Random Guess/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Random_Guesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int guess = 0;
+            int numOfGuesses = 0;
+
+            int rand = rnd.Next(1, 101);
+
+            while (guess != rand)
+            {
+                numOfGuesses++;
+                Console.WriteLine("Guess a number between 1 and 100");
+
+                if (int.TryParse(Console.ReadLine(), out guess))
+                {
+                    if (guess == rand)
+                    {
+                        Console.WriteLine("Correct guess");
+                        Console.WriteLine($"You guessed {numOfGuesses} times");
+                    }
+                    else if (guess < rand)
+                    {
+                        Console.WriteLine("Wrong guess, too low");
+                    }
+                    else if (guess > rand)
+                    {
+                        Console.WriteLine("Wrong guess, too high");
+                    }
+                    Console.WriteLine($"You guessed {guess}");
+                }
+                else
+                {
+                    Console.WriteLine("Not a number");
+                }
+            }
+        }
+    }
+}
